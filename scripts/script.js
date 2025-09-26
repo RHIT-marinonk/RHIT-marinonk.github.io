40c182890bbe26f0ef9ace71e53e2e7b77895070
RHIT-marinonk <marinonk@rose-hulman.edu>
2025-09-26 10:49:29 -0400

    implemented footer

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
-let pages = {"Home":"https://rhit-marinonk.github.io/", "Resume":"resume.html", "Portfolio":"portfolio.html"};
+let navbarMap = {"Home":"https://rhit-marinonk.github.io/", "Resume":"resume.html", "Portfolio":"portfolio.html"};
+let footerMap = {"Linkedin":"resume.html", "Handshake":"resume.html"}
 
-buildNavBar();
+buildHTML("#navbar-placeholder", navbarMap);
+buildHTML("#footer-placeholder", footerMap);
 
 //idea from: Professor Stouder (thank you)
-function buildNavBar() {
-    let oldelem = document.querySelector("#navbar-placeholder");
+function buildHTML(placeholder, aMap) {
+    let oldelem = document.querySelector(placeholder);
     let newelem = document.createElement("div");
     newelem.appendChild(document.createElement("ul"));
-    for (let element in pages) {
+    for (let element in aMap) {
         let li = document.createElement("li");
         newelem.appendChild(li);
-        let page = li.appendChild(document.createElement("a"));
-        page.textContent = element;
-        console.log(pages[element]);
-        page.href = pages[element];
-        console.log(page.href);
+        let a = li.appendChild(document.createElement("a"));
+        a.textContent = element;
+        a.href = aMap[element];
     }
     newelem.appendChild(document.createElement("hr"));
     newelem.appendChild(document.createElement("hr"));
