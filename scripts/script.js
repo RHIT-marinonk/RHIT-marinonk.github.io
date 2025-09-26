f38bf23ca7be2c8a030066293b4ce0c16fe2e866
RHIT-marinonk <marinonk@rose-hulman.edu>
2025-09-26 17:29:14 -0400

    linked linkedin

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 let navbarMap = {"Home":"https://rhit-marinonk.github.io/", "Resume":"resume.html", "Portfolio":"portfolio.html"};
-let footerMap = {"Linkedin":"www.linkedin.com/in/noah-marino-427b22239", "Handshake":"https://app.joinhandshake.com/profiles/ft4gvf"}
+let footerMap = {"Linkedin":"https://www.linkedin.com/in/noah-marino-427b22239", "Handshake":"https://app.joinhandshake.com/profiles/ft4gvf"}
 
 document.addEventListener("DOMContentLoaded", () => {
     buildHTML("#navbar-placeholder", navbarMap);
