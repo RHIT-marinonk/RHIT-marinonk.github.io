3202266c0c0536d3160c6aa4e75dcfdbd547d414
RHIT-marinonk <marinonk@rose-hulman.edu>
2025-09-26 01:39:42 -0400

    maybe final fix?

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,15 @@
-function loadHTML(selector, file) {
+function loadHTMLFile(id, file) {
   fetch(file)
     .then(response => {
       return response.text();
     })
     .then(data => {
-      document.querySelector(selector).innerHTML = data;
-    });
+      document.getElementById(id).innerHTML = data;
+    })
 }
 
-// Load navbar and footer
 document.addEventListener("DOMContentLoaded", () => {
   loadHTML("#navbar-placeholder", "/navbar.html");
   loadHTML("#footer-placeholder", "/footer.html");
 });
+
