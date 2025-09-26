7e3d485b4b1cfdeab3ffa8e1da7ce0d678a6f59a
RHIT-marinonk <marinonk@rose-hulman.edu>
2025-09-26 09:13:42 -0400

    new navbar method

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,36 @@
-//function loadHTMLFile(id, file) {
-//  fetch(file)
-//    .then(response => {
-//      return response.text();
-//    })
-//    .then(data => {
-//      document.getElementById(id).innerHTML = data;
-//    })
-//}
+//idea from: https://stackoverflow.com/questions/31954089/how-can-i-reuse-a-navigation-bar-on-multiple-pages
+// function loadHTMLFile(select, file) {
+//     fetch(file)
+//     .then(res => res.text())
+//     .then(text => {
+//         let oldelem = document.querySelector(select);
+//         let newelem = document.createElement("div");
+//         newelem.innerHTML = text;
+//         oldelem.parentNode.replaceChild(newelem, oldelem)
+//     })
+// }
 
-function loadHTMLFile(select, file) {
-    fetch(file)
-    .then(res => res.text())
-    .then(text => {
-        let oldelem = document.querySelector(select);
-        let newelem = document.createElement("div");
-        newelem.innerHTML = text;
-        oldelem.parentNode.replaceChild(newelem, oldelem)
-    })
+// document.addEventListener("DOMContentLoaded", () => {
+//   loadHTMLFile("#navbar-placeholder", "/navbar.html");
+//   loadHTMLFile("#footer-placeholder", "/footer.html");
+// });
+
+let pages = {"Home":"https://rhit-marinonk.github.io/", "Resume":"resume.html", "Portfolio":"portfolio.html"};
+
+buildNavBar();
+
+function buildNavBar() {
+    let oldelem = document.querySelector("#navbar-placeholder");
+    let newelem = document.createElement("div");
+    newelem.appendChild(document.createElement("ul"))
+    pages.forEach(element => {
+        let page = document.createElement("li");
+        page.appendChild("a")
+        page[0].value = element[0];
+        page[0].href = element[1];
+        newelem.appendChild(page)
+    });
+    newelem.appendChild(document.createElement("hr"))
+    newelem.appendChild(document.createElement("hr"))
+    oldelem.parentNode.replaceChild(newelem, oldelem)
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  loadHTMLFile("#navbar-placeholder", "/navbar.html");
-  loadHTMLFile("#footer-placeholder", "/footer.html");
-});
-
