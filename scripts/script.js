16845f6115206b397afe8d68cd3f7e46836becff
RHIT-marinonk <marinonk@rose-hulman.edu>
2025-09-26 21:50:12 -0400

    more css

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,15 @@ let navbarMap = {"Home":"https://rhit-marinonk.github.io/", "Resume":"resume.htm
 let footerMap = {"Linkedin":"https://www.linkedin.com/in/noah-marino-427b22239", "Handshake":"https://app.joinhandshake.com/profiles/ft4gvf"}
 
 document.addEventListener("DOMContentLoaded", () => {
-    buildHTML("#navbar-placeholder", navbarMap);
-    buildHTML("#footer-placeholder", footerMap);
+    buildHTML("#navbar-placeholder", navbarMap, "navbar");
+    buildHTML("#footer-placeholder", footerMap, "footer");
 })
 
 //idea from: Professor Stouder (thank you)
-function buildHTML(placeholder, aMap) {
+function buildHTML(placeholder, aMap, classname) {
     let oldelem = document.querySelector(placeholder);
     let newelem = document.createElement("div");
-    newelem.classList.add("navbar")
+    newelem.classList.add(classname)
     let ul = newelem.appendChild(document.createElement("ul"));
     for (let element in aMap) {
         let li = document.createElement("li");
